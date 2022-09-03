f9c47aadd2122b3a0007c31c3cbe2a63b8d1b3eb
klaus-moser <klaus-moser@posteo.de>
2022-09-03 21:28:33 +0200

    Add component: flight-search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
 
 @NgModule({
    imports: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
    declarations: [
       AppComponent,
       SidebarComponent,
-      NavbarComponent
+      NavbarComponent,
+      FlightSearchComponent
    ],
    providers: [],
    bootstrap: [
